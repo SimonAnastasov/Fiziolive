9e03a2ca34d7d9f72ca15f46c6859a49b0d31793
SimonAnastasov <simonanastasov2001@gmail.com>
2021-10-08 14:00:33 +0200

    Commented out the connection to mongoDB because no URI present

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
+// import clientPromise from '../lib/mongodb'
 
 import Welcome from '../components/app/Welcome'
 import Landing from '../components/app/Landing'
@@ -29,18 +29,12 @@ export default function Home({ isConnected }) {
   )
 }
 
-export async function getServerSideProps(context) {
-  const client = await clientPromise
+// export async function getServerSideProps(context) {
+//   const client = await clientPromise
 
-  // client.db() will be the default database passed in the MONGODB_URI
-  // You can change the database by calling the client.db() function and specifying a database like:
-  // const db = client.db("myDatabase");
-  // Then you can execute queries against your database like so:
-  // db.find({}) or any of the MongoDB Node Driver commands
+//   const isConnected = await client.isConnected()
 
-  const isConnected = await client.isConnected()
-
-  return {
-    props: { isConnected },
-  }
-}
+//   return {
+//     props: { isConnected },
+//   }
+// }
